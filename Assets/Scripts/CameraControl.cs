64e09ff9074fd7fa9d79333d5764e189b20bd336
jryang36 <jry36@drexel.edu>
2024-04-24 23:37:50 -0400

    camera script and placement

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform[] targets;
+    public Vector3 offset = new Vector3(0, 10, 0);
+    public float smoothTime = 0.5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Length == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Length; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Length == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 1; i < targets.Length; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
