48927fd6088032216ad5f9cb7617068470d51993
SpiffierLyric <56176973+SpiffierLyric@users.noreply.github.com>
2024-05-30 13:09:32 -0400

    Controller Serialization

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
@@ -10,6 +11,24 @@ public class PlayerManager : MonoBehaviour
     private int playerCount = 0;
     public Camera followCam;
     public GameObject[] playerUi;
+    private List<PlayerConfiguration> playerConfigs;
+
+    [SerializeField]
+    public static PlayerManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("[Singleton] Trying to instantiate a second instance of a singleton class.");
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            playerConfigs = new List<PlayerConfiguration>();
+        }
+    }
 
     void Start()
     {
@@ -20,13 +39,24 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    public void HandlePlayerJoined(PlayerInput playerInput)
+    public void HandlePlayerJoined(PlayerInput pi)
     {
-        playerCount++;
-        playerInput.gameObject.name = "Player" + playerCount;
-        followCam.GetComponent<CameraControl>().AddTarget(playerInput.gameObject);   
-        EnableUIElements(playerInput.gameObject.name);
-        EnableHealthIcons(playerInput.gameObject);
+        Debug.Log("player joined " + pi.playerIndex);
+        //pi.transform.SetParent(transform);
+
+        if (!playerConfigs.Any(p => p.PlayerIndex == pi.playerIndex))
+        {
+            playerConfigs.Add(new PlayerConfiguration(pi));
+            playerCount++;
+            pi.gameObject.name = "Player" + playerCount;
+        }
+        followCam.GetComponent<CameraControl>().AddTarget(pi.gameObject);
+            EnableUIElements(pi.gameObject.name);
+            EnableHealthIcons(pi.gameObject);
+    }
+    public List<PlayerConfiguration> GetPlayerConfigs()
+    {
+        return playerConfigs;
     }
 
     void OnDestroy()
@@ -84,4 +114,17 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
+}
+
+public class PlayerConfiguration
+{
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        PlayerIndex = pi.playerIndex;
+        Input = pi;
+    }
+
+    public PlayerInput Input { get; private set; }
+    public int PlayerIndex { get; private set; }
+    public Material playerMaterial {get; set;}
 }
